83c5bda8d4a4a28fb34869587b5dc7c364752f50
Serhii Vovchenko <serega.vovchenko@gmail.com>
2025-03-14 18:39:29 +0100

    add header and hero

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('Ok');
+const openMenuBtn = document.querySelector('.header_burger_btn');
+const mobileMenu = document.querySelector('.nav_block');
+
+openMenuBtn.addEventListener('click', () => {
+	if (mobileMenu.classList.contains('nav_block_active')) {
+		mobileMenu.classList.remove('nav_block_active');
+		openMenuBtn.classList.remove('active_burger_btn');
+	} else {
+		mobileMenu.classList.add('nav_block_active');
+		openMenuBtn.classList.add('active_burger_btn');
+	}
+});
